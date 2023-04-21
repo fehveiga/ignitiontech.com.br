850b5eeacbc9039be72bc3c6f76f852b72ea6a7c
fehveiga <127157240+fehveiga@users.noreply.github.com>
2023-04-20 22:46:14 -0300

    Initial commit

---
+++ arquivos/vfm-admin/_content/users/users.php
@@ -0,0 +1,19 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$ALJSlxIm$FiuQw0ByLwpI2kstSpTLL0',
+    'role' => 'superadmin',
+    'email' => 'fehveiga@gmail.com',
+  ),
+  1 => 
+  array (
+    'name' => 'fehveiga',
+    'pass' => '$1$ciu9Ng6f$/oUxddeezFIcZd68/Dn9P/',
+    'role' => 'admin',
+    'disabled' => false,
+    'email' => 'fehveiga@hotmail.com.br',
+  ),
+);
